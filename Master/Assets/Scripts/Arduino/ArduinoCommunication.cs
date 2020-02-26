3df20a64afbf9544b33bcce19a225eabfa184e9d
lassejoe <info@lassejohansen.dk>
2020-02-26 15:47:29 +0100

    main communication between arduino and unity

--- Master/Assets/Scripts/Arduino/ArduinoCommunication.cs
+++ Master/Assets/Scripts/Arduino/ArduinoCommunication.cs
@@ -1,24 +1,58 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO.Ports;
+using UnityEngine.Serialization;
 
 public class ArduinoCommunication : MonoBehaviour {
 
-    SerialPort sp = new SerialPort("COM5", 9600);
- 
-    void Start () {
-        sp.Open ();
-        sp.ReadTimeout = 1;
-    }
- 
-    void Update () 
+    SerialPort stream = new SerialPort("COM5", 9600,Parity.Even,7,StopBits.One);
+    private Player_handler _player;
+    private string dataStream = "";
+    public bool SwapIT = false;
+    public string id = "";
+    public string swapId = "";
+    void Start()
     {
-        try{
-            print (sp.ReadLine());
+        stream.ReadTimeout = 1;
+        stream.Open(); //Open the Serial Stream.
+        _player = new Player_handler();
+    }
+
+    void Update()
+    {
+        try
+        {
+            dataStream = stream.ReadExisting();
+            if (!dataStream.Equals("") && !dataStream.Equals(id))
+            {
+                id = dataStream;
+                _player.AddBlock(id);
+            }
         }
-        catch(Exception ex){
-            Debug.Log(ex);
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        TriggerSwap();
+    }
+
+    void TriggerSwap()
+    {
+        if (SwapIT)
+        {
+            _player.SwapBlocks(swapId,"down");
+            SwapIT = !SwapIT;
+        }
+    }
+    
+    private void OnApplicationQuit()
+    {
+        if (stream.IsOpen)
+        {
+            stream.Close();
         }
     }
 }
---
+++ Master/Assets/Scripts/Moving_block/Player_handler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Player_handler : MonoBehaviour
+{
+    public Player_handler()
+    {
+        
+    }
+    private List<Block> listOfBlock = new List<Block>();
+    private int maxAmountOfBlocks = 4;
+
+    public void AddBlock(string id)
+    {
+        //Had to trim this fucking string - caused random errors. Fuck unity.
+        string input = id.Trim();
+        if (!ContainsElement(input) && listOfBlock.Count < maxAmountOfBlocks)
+        {
+            string state = DetermineMove();
+            Block _block = new Block(input, state);
+            listOfBlock.Add(_block);
+            print($"added block with id {input} and it's state is: {state}");
+            
+        }
+    }
+
+    public void SwapBlocks(string id, string state)
+    {
+        foreach (var block in listOfBlock.Where(block => block.Id.Equals(id)))
+        {
+            block.CurrentState = state;
+        }
+    }
+
+    private bool ContainsElement(string _id)
+    {
+        return listOfBlock.Any(block => _id.Equals(block.Id));
+    }
+
+    private string DetermineMove()
+    {
+        switch (listOfBlock.Count)
+        {
+            case 0: return "up";
+            case 1: return "right";
+            case 2: return "down";
+            case 3: return "left";
+            default: return null;
+        }
+    }
+}
