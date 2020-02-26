226036e61956f896827bcbc16a13df0ebf7dfcda
lassejoe <info@lassejohansen.dk>
2020-02-26 09:46:18 +0100

    add arduino serial communication

---
+++ Master/Assets/Scripts/Arduino/ArduinoCommunication.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO.Ports;
+
+public class ArduinoCommunication : MonoBehaviour {
+
+    SerialPort sp = new SerialPort("COM5", 9600);
+ 
+    void Start () {
+        sp.Open ();
+        sp.ReadTimeout = 1;
+    }
+ 
+    void Update () 
+    {
+        try{
+            print (sp.ReadLine());
+        }
+        catch(Exception ex){
+            Debug.Log(ex);
+        }
+    }
+}
