ed361d3b7553f1064a454da1810e58ad80d1f3ac
MathiasLaptop <mathiasmundtkofod@gmail.com>
2020-03-05 15:17:21 +0100

    Implemented Networking
    Didn't implment it directly to the game, but only in some test scenes. The scenes are working, and players connecting are able to play around with their respective avatars

---
+++ Master/Assets/Scripts/Networking/QuickStartLobbyController.cs
@@ -0,0 +1,56 @@
+﻿using Photon.Pun;
+using Photon.Pun.Demo.Cockpit.Forms;
+using Photon.Realtime;
+using UnityEngine;
+
+public class QuickStartLobbyController : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject quickStartBtn;
+
+    [SerializeField] private GameObject quickCancelBtn;
+
+    public int roomSize;
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        quickStartBtn.SetActive(true);
+    }
+
+    public void QuickStart()
+    {
+        quickStartBtn.SetActive(false);
+        quickCancelBtn.SetActive(true);
+
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Quick start");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join a room");
+        CreateRoom();
+    }
+
+    private void CreateRoom()
+    {
+        Debug.Log("Creating new room");
+        int randomNumber = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions(){IsVisible = true,IsOpen = true,MaxPlayers = (byte) roomSize};
+        PhotonNetwork.CreateRoom("Room" + randomNumber, roomOps);
+        Debug.Log(randomNumber);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create room, trying again");
+        CreateRoom();
+    }
+
+    public void QuickCancel()
+    {
+        quickCancelBtn.SetActive(false);
+        quickStartBtn.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
